e0fd4957aabaca98733700df4feb1a79acd996a1
Prosenjit Mondal <prosenjit.contact.me@gmail.com>
2019-03-27 23:16:57 +0600

    setup start script and heroku port

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
